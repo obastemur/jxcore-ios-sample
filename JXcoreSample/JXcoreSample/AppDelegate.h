871af6fece0750ab804880c114433e4d602cd5d8
Oguz Bastemur <obastemur@gmail.com>
2015-06-21 19:42:13 +0200

    initial dummy project

---
+++ JXcoreSample/JXcoreSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXcoreSample
+//
+//  Created by Gonzo on 6/21/15.
+//  Copyright (c) 2015 Nubisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXcoreSample/JXcoreSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXcoreSample
+//
+//  Created by Gonzo on 6/21/15.
+//  Copyright (c) 2015 Nubisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
